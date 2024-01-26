9bd3993d4c60184f338cd344f23a2997cb77aae3
leonardobaranelli <leobaranelli@gmail.com>
2024-01-26 13:19:47 -0300

    feat: db setup with sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { config } from 'dotenv';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',     
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
